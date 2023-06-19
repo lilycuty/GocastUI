8dd4b7110fbd6426dcb334f20fadc390c70c2c33
THAI <xinphatmutikill@gmail.com>
2023-06-20 04:18:46 +0700

    Update GocastUI - Home

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
 'use strict';
 const btnMenu = document.querySelector('.button-menu');
 const sideBar = document.querySelector('.sidebar');
-const btnUnion = document.querySelector('.button-union');
-const mainLeft = document.querySelector('.main-content-left');
-const mainRight = document.querySelector('.main-content-right');
-const body = document.querySelector('body');
+
 btnMenu.addEventListener('click', function () {
 	sideBar.classList.toggle('active');
-});
-
-btnUnion.addEventListener('click', function () {
-	mainLeft.classList.toggle('hidden');
-	mainRight.classList.toggle('hidden');
 });
